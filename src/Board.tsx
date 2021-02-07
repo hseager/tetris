4623241e274ae6341d0dff46fa4a0df8a1934cec
hseager <harvey.seager@googlemail.com>
2021-02-07 17:52:26 +0000

    Started drawGrid

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,21 +1,25 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function Board() {
     const canvasRef = useRef<HTMLCanvasElement>(null)
+
+    const columns = useState(10)
+    const rows = useState(20)
+
+    const drawGrid = (ctx: CanvasRenderingContext2D) => {
+
+    }
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas?.getContext('2d')
         if(canvas && context){
-            context.fillRect(0, 0, canvas.width, canvas.height)
-            context.fillStyle = '#fff'
+            drawGrid(context)
         }
-    })
+    },[])
 
     return (
-        <>
-            <canvas ref={canvasRef} width="200" height="400" className="bg-gray-200 border-solid border-4 border-gray-300" />
-        </>
+        <canvas ref={canvasRef} width="200" height="400" className="border-solid border-4 border-gray-300" />
     )
 }
 
