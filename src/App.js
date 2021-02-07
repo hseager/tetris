55c68111fd3651bc09f59d0dc7d256edeb74d382
hseager <harvey.seager@googlemail.com>
2021-02-07 11:05:48 +0000

    Installed TailwindCss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <h2>Hello world</h2>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h2>Hello world</h2>
+        </div>
+    );
 }
 
 export default App;
