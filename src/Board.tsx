41bdf9deadf35d8a9ccf00e7ac31daf174bdb26f
hseager <harvey.seager@googlemail.com>
2021-02-09 14:56:29 +0000

    Added grid

--- src/Board.tsx
+++ src/Board.tsx
@@ -4,25 +4,28 @@ function Board() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null)
 
     const [columns, setColumns] = useState(10)
-    const [rows, setRows] = useState(20)
     const [width, setWidth] = useState(200)
     const [height, setHeight] = useState(400)
     const [blockSize, setBlockSize] = useState(width / columns)
 
-    const canvasOffset = 0.5
-
-    const drawLine = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
-        ctx.moveTo(x, 0)
-        ctx.lineTo(x, height + canvasOffset)
-        ctx.strokeStyle = '#d1d5db'
+    const drawLine = (ctx: CanvasRenderingContext2D, x: number, y: number, length: number , type: 'horizontal'|'vertical') => {
+        ctx.beginPath()
+        ctx.moveTo(x, y)
+        if(type === 'vertical')
+            ctx.lineTo(x, length)
+        else
+            ctx.lineTo(length, y)
+        ctx.strokeStyle = '#ddd'
         ctx.stroke()
     }
 
     const drawGrid = (ctx: CanvasRenderingContext2D) => {
         for(let lineY = 0; lineY <= width; lineY += blockSize){
-            drawLine(ctx, lineY, 0)
+            drawLine(ctx, lineY, 0, height, 'vertical')
         }
-        
+        for(let lineX = 0; lineX <= height; lineX += blockSize){
+            drawLine(ctx, 0, lineX, width, 'horizontal')
+        }
     }
 
     useEffect(() => {
@@ -34,7 +37,7 @@ function Board() {
     },[])
 
     return (
-        <canvas ref={canvasRef} width={width} height={height} className="border-solid border-4 border-gray-300" />
+        <canvas ref={canvasRef} width={width} height={height} className="border-solid border-8 border-gray-300 bg-white rounded-md" />
     )
 }
 
