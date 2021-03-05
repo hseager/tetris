8a35979de2c29deda38fd15ebfbd6494a8598dd2
hseager <harvey.seager@googlemail.com>
2021-03-05 16:03:40 +0000

    Updated floor collision

--- src/classes/CollisionDetection.tsx
+++ src/classes/CollisionDetection.tsx
@@ -1,8 +1,9 @@
 import Position from './Position'
 import Shape from './Shape'
+const clone = require('lodash/cloneDeep')
 
 class CollisionDetection {
-    static collidingWithPile(nextMove: Position, currentShape: Shape, pile: Array<Shape>): boolean{
+    static collidingWithPile(nextMove: Position, currentShape: Shape, pile: Array<Shape>): boolean {
         if(pile.length === 0) return false
         let colliding = false
         pile.forEach(pileShape => {
@@ -13,8 +14,11 @@ class CollisionDetection {
         })
         return colliding
     }
-    static collidingWithFloor(nextMove: Position, currentShape: Shape, height: number): boolean{
-        if(currentShape.blocks.some(block => nextMove.y && block.position.y + (nextMove.y - currentShape.position.y) >= height))
+    static collidingWithFloor(nextMove: Position, currentShape: Shape, height: number): boolean {
+        let testShape: Shape = clone(currentShape)
+        testShape.update(nextMove)
+        
+        if(testShape.blocks.some(block => block.position.y >= height))
             return true
         return false
     }
